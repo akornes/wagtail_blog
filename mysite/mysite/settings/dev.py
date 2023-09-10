686a8da45e594c38834fd6fff5299e582c354533
akornes <andrewkornes@gmail.com>
2023-09-09 20:20:11 -0400

    first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(&$_!u0h&98_qdhn)qmx-4!^+yb2b@2b2sunkvq_ymv7nod+fe"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
